2949bc5ae72627e6b0e1ab22dfe83543d0702b62
ksamuel <info.ksamuel@gmail.com>
2010-09-24 12:30:19 +0200

    CSv generation and better translation

--- who_base/views.py
+++ who_base/views.py
@@ -2,14 +2,18 @@
 # encoding=utf-8
 # vim: ai ts=4 sts=4 et sw=4
 
+import csv
+
 from itertools import groupby
 from operator import attrgetter
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
-
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext as _
 
 from .models import Campaign, Results
 
@@ -32,6 +36,54 @@ def dashboard(request):
         page = paginator.page(paginator.num_pages)
         if page.object_list:
             campaign = page.object_list[0]
+            
+    if request.GET.get('format', None) == 'csv':
+
+        response = HttpResponse(mimetype='text/csv')
+        name = slugify(unicode(campaign))
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % name
+
+        class CustomCSV(csv.excel):
+            delimiter = ";"
+
+        csv.register_dialect("CustomCSV", CustomCSV)
+
+        writer = csv.writer(response, CustomCSV)
+        
+        convert_to_unicode = lambda l: [unicode(x if x is not None else '').encode('utf8') for x in l]
+        
+        writer.writerow(convert_to_unicode([_(u'Region'), _(u'Cercle'),
+                         _(u'City'), _(u'Village'),
+                        _(u'Drugs'), _(u'Distributor'), _(u'Treated on'), 
+                        _(u'Reported on'),
+                        _(u'1-4 years old males'), _(u'5-14 years old males'),
+                         _(u'15+ years old males'),
+                        _(u'1-4 years old females'), _(u'5-14 years old females'),
+                         _(u'15+ years old females')]))
+         
+        for result in Results.objects.filter(campaign=campaign)\
+                                     .order_by('area__parent__parent__parent__name', 
+                                               'area__parent__parent__name',
+                                               'area__parent__name'):
+            writer.writerow(convert_to_unicode(
+                             [result.area.parent.parent.parent.name, 
+                             result.area.parent.parent.name, 
+                             result.area.parent.name, 
+                             result.area.name,
+                             ', '.join(d.name for d in result.pack.drugs.all()),
+                             result.distributor,
+                             result.area.name, 
+                             result.treatment_date,
+                             result.report_date,
+                             result.child_males_data,
+                             result.teen_males_data,
+                             result.adult_males_data,
+                             result.child_females_data,
+                             result.teen_females_data,
+                             result.adult_females_data,]))
+
+        return response
+
 
     if campaign:
         
@@ -53,9 +105,6 @@ def dashboard(request):
             data['count'] = data.get('count', 0) + 1
             data.setdefault('cercles', []).append((cercle, cercle_data))
             
-        import pprint
-        pprint.pprint(cercles_by_region)
-
     ctx = locals()
 
     return render_to_response('who_base/index.html',  ctx,
